f54b5cb1fb9088bce3df7549f827c681c09027fc
Patrick <31260874+patwer92@users.noreply.github.com>
2021-12-13 23:20:17 +0100

    added first commit

--- config/database.js
+++ config/database.js
@@ -1,15 +1,36 @@
+// module.exports = ({ env }) => ({
+//   defaultConnection: "default",
+//   connections: {
+//     default: {
+//       connector: "mongoose",
+//       settings: {
+//         host: env("DATABASE_HOST"),
+//         srv: env.bool("DATABASE_SRV", true),
+//         port: env.int("DATABASE_PORT", 27017),
+//         database: env("DATABASE_NAME"),
+//         username: env("DATABASE_USERNAME"),
+//         password: env("DATABASE_PASSWORD"),
+//       },
+//       options: {
+//         authenticationDatabase: env("AUTHENTICATION_DATABASE", null),
+//         ssl: env.bool("DATABASE_SSL", true),
+//       },
+//     },
+//   },
+// });
+
 module.exports = ({ env }) => ({
   defaultConnection: "default",
   connections: {
     default: {
       connector: "mongoose",
       settings: {
-        host: env("DATABASE_HOST"),
+        host: env("DATABASE_HOST", "strapi-api.hjuih.mongodb.net"),
         srv: env.bool("DATABASE_SRV", true),
         port: env.int("DATABASE_PORT", 27017),
-        database: env("DATABASE_NAME"),
-        username: env("DATABASE_USERNAME"),
-        password: env("DATABASE_PASSWORD"),
+        database: env("DATABASE_NAME", "strapi-api"),
+        username: env("DATABASE_USERNAME", "patrickwerner"),
+        password: env("DATABASE_PASSWORD", "admin123"),
       },
       options: {
         authenticationDatabase: env("AUTHENTICATION_DATABASE", null),
